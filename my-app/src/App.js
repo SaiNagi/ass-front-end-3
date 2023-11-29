555965e9ce27f7f9c58b311c52c216506d26ed8c
SaiNagi <56darling.sai@gmail.com>
2023-11-29 15:05:04 +0530

    Remove submodule 'my-app'

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Posts from "./components/Posts";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
