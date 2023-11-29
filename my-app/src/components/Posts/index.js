3b51efc49b1dbde1464ecff8f1dfe16a289d4727
SaiNagi <131807462+SaiNagi@users.noreply.github.com>
2023-11-29 16:52:55 +0530

    Update index.js

--- my-app/src/components/Posts/index.js
+++ my-app/src/components/Posts/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "./index.css";
 
 class Posts extends Component {
-  state = { data: [] };
+  state = {
+    data: [],
+    isLoading: true, // Added loading state
+  };
 
   componentDidMount() {
     this.getData();
@@ -15,34 +20,43 @@ class Posts extends Component {
       const posts = await response.json();
       console.log(posts);
 
-      // Update the state with the fetched data
-      this.setState({ data: posts });
+      
+      this.setState({ data: posts, isLoading: false });
     } catch (error) {
       console.error("Error fetching data:", error);
+
+      
+      this.setState({ isLoading: false });
     }
   };
 
   render() {
-    const { data } = this.state;
+    const { data, isLoading } = this.state;
+
     return (
-      <div>
-        <ul>
-          {data.map((item) => (
-            <li className="list-item" key={uuidv4()}>
-              <h3>{item.title}</h3>
-              <p>
-                {item.content.slice(0, 100)}
-                {item.content.length > 20 ? "..." : ""}
-              </p>
-              <p>
-                <span className="posted-by">Posted by</span> : {item.user_name}
-              </p>
-              <button className="view-btn" type="button">
-                View Post
-              </button>
-            </li>
-          ))}
-        </ul>
+      <div className="loader">
+        {isLoading ? ( 
+          <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+        ) : (
+          <ul>
+            {data.map((item) => (
+              <li className="list-item" key={uuidv4()}>
+                <h3>{item.title}</h3>
+                <p>
+                  {item.content.slice(0, 100)}
+                  {item.content.length > 20 ? "..." : ""}
+                </p>
+                <p>
+                  <span className="posted-by">Posted by</span> :{" "}
+                  {item.user_name}
+                </p>
+                <button className="view-btn" type="button">
+                  View Post
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
